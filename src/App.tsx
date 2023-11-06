66c9f438e211b3dca40922ef584c62c699d15744
benwhite0820 <97225493+benwhite0820@users.noreply.github.com>
2023-11-06 12:23:21 +0800

    building the overrall layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import './App.scss';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="container">
+        <header className="header">Header part</header>
+        <div className="content">
+          <nav className="sidebar">Navigation</nav>
+          <main className="hotel-view">Hotel view</main>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
