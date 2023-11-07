b6a97eaca984bab50eccf6b107bba060df26d30e
benwhite0820 <dragon7230@gmail.com>
2023-11-07 16:58:36 +0800

    building the description section - part 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import Gallery from '@components/gallery/gallery.component';
 import './App.scss';
+import Gallery from '@components/gallery/gallery.component';
 import Header from '@components/header/header.component';
 import SideBar from '@components/side-bar/side-bar.component';
 import Overview from '@components/overview/overview.component';
-import Detail from '@components/detail/detail.component';
+import Description from '@components/description/description.component';
+import UserReviews from '@components/user-reviews/user-reviews.component';
 
 function App() {
   return (
@@ -15,7 +16,10 @@ function App() {
           <main className="hotel-view">
             <Gallery />
             <Overview />
-            <Detail />
+            <div className="detail">
+              <Description />
+              <UserReviews />
+            </div>
           </main>
         </div>
       </div>
---
+++ src/components/user-reviews/user-reviews.component.tsx
@@ -0,0 +1,7 @@
+import './user-reviews.style.scss';
+
+const UserReviews = () => {
+  return <div className="user-reviews">User reviews</div>;
+};
+
+export default UserReviews;
